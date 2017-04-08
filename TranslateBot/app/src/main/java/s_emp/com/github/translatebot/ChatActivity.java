c54c38edfe73b62f17408d5bc82538de22e8fa5b
Сергей Мельников <emp@MacBook-Pro-Sergej.local>
2017-04-08 14:33:42 +0300

    Создан проект

---
+++ TranslateBot/app/src/main/java/s_emp/com/github/translatebot/ChatActivity.java
@@ -0,0 +1,13 @@
+package s_emp.com.github.translatebot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
